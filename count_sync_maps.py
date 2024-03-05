1e91d7d0032cef646366257a4051d8d62cfebc0f
Maximilian Boehm <63911020+maximilianboehm@users.noreply.github.com>
2024-03-05 23:26:03 +0100

    Update count_sync_maps.py

--- count_sync_maps.py
+++ count_sync_maps.py
@@ -5,32 +5,35 @@ count = 0
 count_all = 0
 root_folder = "."
 valid_folders = []
+
 for folder in os.listdir(root_folder):
     if folder.startswith("scripts"):
-        for sub_folder in os.listdir(os.path.join(root_folder, folder)):
-            if sub_folder == "data":
-                for data_folder in os.listdir(os.path.join(root_folder, folder, "data")):
-                    syncmap_exists = False
-                    mp4_exists = False
-                    for f in os.listdir(os.path.join(root_folder, folder, "data", data_folder)):
-                        if f == "syncmap.json":
-                            print(os.path.join(root_folder, folder, "data", data_folder,f))
-                            syncmap_exists = True
-                        if f.endswith(".mp4"):
-                            print(os.path.join(root_folder, folder, "data", data_folder,f))
-                            mp4_exists = True
-                    if syncmap_exists and mp4_exists:
-                        valid_folders.append(os.path.join(root_folder, folder, "data", data_folder))
-                        count += 1
-                    count_all += 1
-                print("")
-                
+        data_path = os.path.join(root_folder, folder, "data")
+        if os.path.exists(data_path):
+            for data_folder in os.listdir(data_path):
+                data_folder_path = os.path.join(data_path, data_folder)
+                syncmap_exists = False
+                mp4_exists = False
+                for f in os.listdir(data_folder_path):
+                    file_path = os.path.join(data_folder_path, f)
+                    if f == "syncmap.json":
+                        print(file_path)
+                        syncmap_exists = True
+                    elif f.endswith(".mp4"):
+                        print(file_path)
+                        mp4_exists = True
+                if syncmap_exists and mp4_exists:
+                    valid_folders.append(data_folder_path)
+                    count += 1
+                count_all += 1
+            print("")
+
 with open("valid_embedding_folders.csv", 'w', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(valid_folders)
-    
-print("")     
+
+print("")
 print(count)
 print(count_all)
-            
+
                 
